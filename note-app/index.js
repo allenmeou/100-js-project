013189d725390222fc50f7d0a30cbbccc99264c6
Tran Dinh Huy <104847278+allenmeou@users.noreply.github.com>
2023-06-23 11:44:28 +0700

    upload code

---
+++ note-app/index.js
@@ -0,0 +1,65 @@
+const btnEl = document.querySelector(".btn");
+const appEl = document.querySelector(".app");
+
+getNotes().forEach((note) => {
+  const noteEl = createNote(note.id, note.content);
+  appEl.insertBefore(noteEl, btnEl);
+});
+
+function createNote(id, content) {
+  const element = document.createElement("textarea");
+  element.classList.add("note");
+  element.placeholder = "Empty Note";
+  element.value = content;
+
+  element.addEventListener("dblclick", () => {
+    const warning = confirm("Do you want to delete this note?");
+    if (warning) {
+      deleteNote(id, element);
+    }
+  });
+  element.addEventListener("input", () => {
+    updateNote(id, element.value);
+  });
+
+  return element;
+}
+
+function updateNote(id, content) {
+  const notes = getNotes();
+  const target = notes.filter((note) => note.id == id)[0];
+  target.content = content;
+  saveNote(notes);
+}
+
+function deleteNote(id, element) {
+  const notes = getNotes().filter((note) => note.id != id);
+  saveNote(notes);
+  appEl.removeChild(element);
+}
+
+function addNote() {
+  const notes = getNotes();
+
+  const noteObj = {
+    id: Math.floor(Math.random() * 100000),
+    content: "",
+  };
+
+  const noteEl = createNote(noteObj.id, noteObj.content);
+  appEl.insertBefore(noteEl, btnEl);
+
+  notes.push(noteObj);
+
+  saveNote(notes);
+}
+
+function saveNote(notes) {
+  localStorage.setItem("note-app", JSON.stringify(notes));
+}
+
+function getNotes() {
+  return JSON.parse(localStorage.getItem("note-app") || "[]");
+}
+
+btnEl.addEventListener("click", addNote);
